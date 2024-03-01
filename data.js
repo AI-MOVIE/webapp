4823b42787c59ec4c7a00e50fb52ca8fc1f2f4e7
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-03-01 13:31:18 +0100

    Update data.js
    update 01

--- data.js
+++ data.js
@@ -1,1 +1,27 @@
-404: Not Found
+let movies = [
+    {
+        name: 'Harry Potter and Volcano Witch',
+        des: 'Harry Potter has a confrontation with a new enemy, the volcano witch!',
+        image: 'images/Harry Volcano3.png'
+    },
+    {
+        name: 'Star War',
+        des: 'Start a battle, the Jedi will have the strength to defeat the dark side!',
+        image: 'images/starwar2.png'
+    },
+    {
+        name: 'T-REX',
+        des: 'A documentary that sees T-REX from all angles!',
+        image: 'images/trex.png'
+    },
+    {
+        name: 'UFC',
+        des: 'A blood sport of struggle and perseverance, not everyone has the nerve to achieve it!',
+        image: 'images/UFC.png'
+    },
+    {
+        name: 'ANGELS AND DEMONS',
+        des: 'The battle between good and evil has begun, which side are you on?',
+        image: 'images/angels.png'
+    }
+]
